b1e4759efab9175c5e750f66f4d4271067701f48
Дмитрий Яснобулков <yasnobulkov.da@dns-shop.ru>
2021-08-10 22:25:28 +0300

    task-2 initial commit

---
+++ cmd/ova-exam-api/internal/utils.go
@@ -0,0 +1,68 @@
+package utils
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println("Hello! It's ova-exam-api project!")
+}
+
+func Div(input []int, chunkSize int) [][]int {
+	if len(input) == 0 || chunkSize < 1 {
+		panic("Incorrect parameters")
+	}
+
+	var floatChunkSize = float64(len(input)) / float64(chunkSize)
+	var size = int(math.Ceil(floatChunkSize))
+	result := make([][]int, size)
+	for i:=0;i<size;i++ {
+		currentChunkStart := i * chunkSize
+		var currentChunkEnd int
+
+		if currentChunkStart + chunkSize < len(input) {
+			currentChunkEnd = currentChunkStart + chunkSize
+		} else {
+			currentChunkEnd = len(input)
+		}
+
+		result[i] = input[currentChunkStart:currentChunkEnd]
+	}
+
+	return result
+}
+
+func Invert(input map[string]string) map[string]string {
+	if input == nil || len(input) == 0{
+		panic("Incorrect parameters")
+	}
+
+	result := make(map[string]string, len(input))
+	for key, value := range input {
+		if _, ok := result[value]; ok {
+			panic("Key value is duplicated ")
+		}
+		result[value] = key
+	}
+
+	return result
+}
+
+
+func Filter(input []string, filter string) []string {
+	if len(input) == 0 || len(filter) == 0 {
+		panic("Incorrect parameters")
+	}
+	
+	result := make([]string, 0)
+	for _, v := range input {
+		if v == filter {
+			continue
+		}
+
+		result = append(result, v)
+	}
+
+	return result
+}
--- cmd/ova-exam-api/main.go
+++ cmd/ova-exam-api/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	utils "ova-exam-api/cmd/ova-exam-api/internal"
+)
 
 func main() {
-	fmt.Println("Hello! It's ova-exam-api project!")
+	source1 := []int{1,2,3,4,5,6,7,8,9,10,11,12}
+	fmt.Println(source1)
+	result1 := utils.Div(source1, 5)
+	fmt.Println(result1)
+
+	source2 := map[string]string{
+		"key1": "value1",
+		"key2": "value2",
+		"key3": "value3",
+		"key4": "value4",
+		"key5": "value5",
+		"key6": "value6",
+	}
+	fmt.Println(source2)
+	result2 := utils.Invert(source2)
+	fmt.Println(result2)
+
+
+	source3 := []string{"слово1", "слово2", "слово3", "слово2", "слово4"}
+	fmt.Println(source3)
+	result3 := utils.Filter(source3, "слово2")
+	fmt.Println(result3)
 }
